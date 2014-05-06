a831bd1305ed2382e07eeaa1eadfbe7da15194f7
matt <matt@matts-Mac.local>
2014-05-06 03:40:43 +0100

    Initial Commit

---
+++ LifeQuest/LQAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LQAppDelegate.h
+//  LifeQuest
+//
+//  Created by matt on 06/05/2014.
+//  Copyright (c) 2014 MattRyder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LQAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LifeQuest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LifeQuest
+//
+//  Created by matt on 06/05/2014.
+//  Copyright (c) 2014 MattRyder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LQAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LQAppDelegate class]));
+    }
+}
